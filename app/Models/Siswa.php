5e4362ec1167b5e9cc7e650970cc3f91dc6b2977
rahmatsigma <rahmattillah079@gmail.com>
2026-02-13 08:06:29 +0700

    idk
    idk

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -7,11 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 class Siswa extends Model
 {
     protected $fillable = ['nis', 'nama_lengkap', 'tanggal_lahir', 'alamat', 'kelas_id'];
+    protected $fillable = ['user_id', 'nis', 'nama_lengkap', 'kelas_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
 // Definisikan relasi ke model Kelas
     public function kelas()
     {
         return $this->belongsTo(Kelas::class);
     }
+
+    public function siswa()
+    {
+        return $this->hasOne(Siswa::class);
+    }
+
 }
 
